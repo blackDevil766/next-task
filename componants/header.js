6bd9bd723e2df3acf4aeaba8840a7a3b2b1e0f8d
ahmed allam <ahmedallam0101560@gmail.com>
2023-05-12 20:45:16 +0300

    updata

--- componants/header.js
+++ componants/header.js
@@ -19,12 +19,12 @@ export default function Header() {
         <h1><span>uiux</span> Labs</h1>
         <div className="nav-items">
           <ul className="items-list">
-            <li onClick={() => setActive( prev=> [...prev, item1 = "active"])} className={active.item1}>home</li>
-            <li onClick={() => setActive( prev=> [...prev, item2 = "active"])} className={active.item2}>collections</li>
-            <li onClick={() => setActive( prev=> [...prev, item3 = "active"])} className={active.item3}>stores</li>
-            <li onClick={() => setActive( prev=> [...prev, item4 = "active"])} className={active.item4}>contact us</li>
-            <li onClick={() => setActive( prev=> [...prev, item5 = "active"])} className={active.item5}>about us</li>
-            <li onClick={() => setActive( prev=> [...prev, item6 = "active"])} className={active.item6}>blog</li>
+            <li onClick={() => setActive( prev => [...prev, item1 = "active"])} className={active.item1}>home</li>
+            <li onClick={() => setActive( prev => [...prev, item2 = "active"])} className={active.item2}>collections</li>
+            <li onClick={() => setActive( prev => [...prev, item3 = "active"])} className={active.item3}>stores</li>
+            <li onClick={() => setActive( prev => [...prev, item4 = "active"])} className={active.item4}>contact us</li>
+            <li onClick={() => setActive( prev => [...prev, item5 = "active"])} className={active.item5}>about us</li>
+            <li onClick={() => setActive( prev => [...prev, item6 = "active"])} className={active.item6}>blog</li>
           </ul>
         </div>
 
